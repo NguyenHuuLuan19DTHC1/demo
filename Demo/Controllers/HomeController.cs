994cbf3da0ae4bf293048fcaeefc6dea7e0a85a9
NguyenHuuLuan19DTHC1 <Ngluan161121@gmail.com>
2023-02-13 22:06:31 +0700

    upload

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult trang_chu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult trang_chu(FormCollection collection)
+        {
+            var mssv = collection["username"];
+            var pass = collection["password"];
+            if (mssv.ToString() == "1234" && pass.ToString() == "1234")
+            {
+                return RedirectToAction("danh_sach", "DSCN");
+            }
+            return View();
+            
+        }
+    }
+}
